39e82d51a8e5712962e1b53ab519c6a00276a602
Daniel Thoma <44621469+djfhe@users.noreply.github.com>
2022-02-16 01:12:44 +0100

    fix login layout (#6)
    * fix login layout
    
    * Update css/login.css
    
    remove whitespace
    
    Co-authored-by: saibotk <git@saibotk.de>
    
    Co-authored-by: saibotk <git@saibotk.de>

--- index.php
+++ index.php
@@ -19,12 +19,15 @@ print_head(array("css/main.css", "css/login.css"), "IAESTE CRM - Login");
 ?>
 
 <div class="loginMain">
-    <p class="loginText" align="center">Sign in</p>
+    <p class="loginText">Sign in</p>
     <img src="<?php echo $logo; ?>"/>
-    <form style="padding-top: 40px;" action="actions/login.php" method="post">
-        <input name="user" class="loginInput" type="text" align="center" placeholder="Username"/>
-        <input name="pw" class="loginInput" type="password" align="center" placeholder="Password"/>
-        <input type="submit" class="loginSubmit" align="center" value="Sign in"/>
+    <form id="login-form" class="loginForm" action="actions/login.php" method="post">
+        <input name="user" class="loginInput" type="text" placeholder="Username"/>
+        <input name="pw" class="loginInput" type="password" placeholder="Password"/>
+    </form>
+    <button type="submit" class="loginSubmit" form="login-form">
+        Sign in
+    </button>
 </div>
 
 <?php
